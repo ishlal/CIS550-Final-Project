dea3a29aa954e3938da717ab23e870537819898c
Andrew Jiang <andrewjiang311@gmail.com>
2022-12-14 23:51:12 -0500

    clutch and luck page tables

--- website/client/src/pages/ClutchPage.js
+++ website/client/src/pages/ClutchPage.js
@@ -6,32 +6,30 @@ import { Table } from 'antd';
 
 function ClutchPage(props) {
     const [clutch, setClutch] = useState({});
-
+    // const dummyData = [{"playerID":1629875,"name":"Xavier Moon","clutch_score":1.4199611688655065,"attempts":10},{"playerID":1626262,"name":"Coty Clarke","clutch_score":1.371230341417251,"attempts":1},{"playerID":2422,"name":"John Salmons","clutch_score":1.3090232392669723,"attempts":2},{"playerID":1630644,"name":"Mac McClung","clutch_score":1.28160802761726,"attempts":2}]
     const tableColumns = [
         {
             title: 'Name',
-            dataIndex: 'Name',
+            dataIndex: 'name',
             key: 'name',
             sorter: (a, b) => a.name.localeCompare(b.name),
-            render: (text, row) => <p>{text}</p>
         },
         {
             title: 'Clutch Index',
-            dataIndex: 'Clutch',
+            dataIndex: 'clutch_score',
             key: 'clutch_score',
             sorter: (a, b) => a.clutch_score - b.clutch_score
     
         },
         {
             title: 'Attempts',
-            dataIndex: 'Attempts',
-            key: 'attemps',
+            dataIndex: 'attempts',
+            key: 'attempts',
             sorter: (a, b) => a.attempts - b.attemps
         }
     ];
 
     useEffect(() => {
-        console.log("in useEffect")
         getClutchPlayers(0).then(res => {
             console.log(res);
             setClutch(res.results)
@@ -42,8 +40,22 @@ function ClutchPage(props) {
         <div>
             <MenuBar/>
             <h1>Clutch Page!</h1>
+            {/* <Table 
+                dataSource={dummyData} 
+                columns={tableColumns} 
+                pagination={{ pageSizeOptions:[5, 10], defaultPageSize: 5, showQuickJumper:true }} 
+                style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }}
+                rowKey="playerID"
+            /> */}
+    
             {Object.keys(clutch).length > 0 &&
-                <Table dataSource={clutch} columns={tableColumns} pagination={{ pageSizeOptions:[5, 10], defaultPageSize: 5, showQuickJumper:true }} style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }}/>
+                <Table 
+                    dataSource={clutch} 
+                    columns={tableColumns} 
+                    pagination={{ pageSizeOptions:[5, 10], defaultPageSize: 5, showQuickJumper:true }} 
+                    style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }}
+                    rowKey="playerID"
+                />
             }
         </div>
     )
--- website/client/src/pages/LuckPage.js
+++ website/client/src/pages/LuckPage.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Table } from 'antd';
 
 import MenuBar from '../components/MenuBar';
+import { getLuckyPerformances } from '../fetcher';
+
 function LuckPage(props) {
+    const [luck, setLuck] = useState({});
+    const tableColumns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            sorter: (a, b) => a.name.localeCompare(b.name),
+        },
+        {
+            title: 'Luck Index',
+            dataIndex: 'luck_index',
+            key: 'luck_index',
+            sorter: (a, b) => a.luck_index - b.luck_index
+    
+        },
+        {
+            title: 'Attempts',
+            dataIndex: 'attempts',
+            key: 'attempts',
+            sorter: (a, b) => a.attempts - b.attemps
+        }
+    ];
+
+    useEffect(() => {
+        getLuckyPerformances().then(res => {
+            setLuck(res.results)
+        });
+    }, [])
+
     return (
         <div>
             <MenuBar/>
             <h1>Luck Page!</h1>
+    
+            {Object.keys(luck).length > 0 &&
+                <Table 
+                    dataSource={luck} 
+                    columns={tableColumns} 
+                    pagination={{ pageSizeOptions:[5, 10], defaultPageSize: 5, showQuickJumper:true }} 
+                    style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }}
+                    rowKey="teamID"
+                />
+            }
         </div>
     )
 }
