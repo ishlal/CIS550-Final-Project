fe21b713340bdfe9adafd53946648aae03e77934
Andrew Jiang <andrewjiang311@gmail.com>
2022-12-14 23:34:12 -0500

    player search and clutch query

--- website/client/src/pages/ClutchPage.js
+++ website/client/src/pages/ClutchPage.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import MenuBar from '../components/MenuBar';
+import { getClutchPlayers } from '../fetcher';
+import { Table } from 'antd';
+
 function ClutchPage(props) {
+    const [clutch, setClutch] = useState({});
+
+    const tableColumns = [
+        {
+            title: 'Name',
+            dataIndex: 'Name',
+            key: 'name',
+            sorter: (a, b) => a.name.localeCompare(b.name),
+            render: (text, row) => <p>{text}</p>
+        },
+        {
+            title: 'Clutch Index',
+            dataIndex: 'Clutch',
+            key: 'clutch_score',
+            sorter: (a, b) => a.clutch_score - b.clutch_score
+    
+        },
+        {
+            title: 'Attempts',
+            dataIndex: 'Attempts',
+            key: 'attemps',
+            sorter: (a, b) => a.attempts - b.attemps
+        }
+    ];
+
+    useEffect(() => {
+        console.log("in useEffect")
+        getClutchPlayers(0).then(res => {
+            console.log(res);
+            setClutch(res.results)
+        });
+    }, [])
+
     return (
         <div>
             <MenuBar/>
             <h1>Clutch Page!</h1>
+            {Object.keys(clutch).length > 0 &&
+                <Table dataSource={clutch} columns={tableColumns} pagination={{ pageSizeOptions:[5, 10], defaultPageSize: 5, showQuickJumper:true }} style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }}/>
+            }
         </div>
     )
 }
--- website/client/src/pages/PlayersPage.js
+++ website/client/src/pages/PlayersPage.js
@@ -1,22 +1,39 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 
 import MenuBar from '../components/MenuBar';
+import { getAllShotsOfPlayer } from '../fetcher';
 function PlayersPage(props) {
     const playerRef = useRef();
+    const [playerInfo, setPlayerInfo] = useState({});
+    
 
     const onSubmit = (e) => {
-        console.log(playerRef.current.value);
+        getAllShotsOfPlayer(playerRef.current.value, null).then(res => {
+            setPlayerInfo(res.results)
+        });
         e.preventDefault();
     }
+
+    useEffect(() => {
+        console.log(playerInfo);
+    }, [playerInfo])
 
     return (
         <div>
             <MenuBar/>
-            <h1>Players Page!</h1>
-            <form onSubmit={onSubmit}>
-                <input type="text" ref={playerRef}/>
-                <input type="submit" value="Search!"/>
-            </form>
+            {Object.keys(playerInfo).length === 0 ? 
+            <div>
+                <h1>Players Page!</h1>
+                <form onSubmit={onSubmit}>
+                    <input type="text" ref={playerRef}/>
+                    <input type="submit" value="Search!"/>
+                </form>
+            </div> :
+            <div>
+                <h1>{playerInfo[0].namePlayer}</h1>
+                <h2>{playerInfo[0].nameTeam}</h2>
+            </div>
+            }
         </div>
     )
 }
